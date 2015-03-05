9cfd333e213c634afb62ad93f9e84fc9aa51d2c1
William Willing <william@williamwilling.com>
2015-03-05 10:50:26 +0100

    Added a basic controller.

---
+++ AspNetMvc.BasicController/Controllers/GreetingsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.BasicController
+{
+    public class GreetingsController : Controller
+    {
+        // You can run this action by going to http://localhost:60563/Greetings/Hello.
+        // (Don't forget to start the project first by clicking on Debug > Start Without Debugging.)
+        public string Hello()
+        {
+            // This is the HTML-code that gets send to the browser. If you right-click on the page in
+            // your browser and select View Page Source (or whatever the option is called in your
+            // browser of choice) you can see that the source of the page is exactly this string.
+            return "<html><body><p>Hello, world!</p></body></html>";
+        }
+
+        // Here's another example. It works exactly the same. You can run this action by going to
+        // http://localhost:60563/Greetings/Goodbye.
+        public string Goodbye()
+        {
+            return "<html><body><p>Goodbye, <em>cruel</em> world!</p></body></html>";
+        }
+    }
+}
